f5745cd4069a0d891052533b7f452adf85467480
Oliver Mak <oliver139.working@gmail.com>
2024-12-22 02:06:27 +0800

    Initialize

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import { Icon } from "@iconify/vue";
-import { createPinia } from "pinia";
 import { createApp } from "vue";
-import pluginVueGtag from "vue-gtag";
 import App from "./App.vue";
-import i18n from "./i18n";
-import router from "./router";
 
 // #region : CSS
 import "@/assets/css/reset.css";
@@ -15,20 +11,8 @@ import "@/assets/css/main.scss";
 
 const app = createApp(App);
 
-// Vue Plugins
-app.use(createPinia());
-app.use(router);
-app.use(i18n);
-
 // Global Component
 app.component("Icon", Icon);
-
-// Google Analytics
-// app.use(pluginVueGtag, {
-//   config: {
-//     id: "###", // Change to yout Google Analytics ID
-//   },
-// }, router);
 
 // Mount it
 app.mount("#app");
--- src/stores/app.ts
+++
@@ -1,8 +0,0 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
-
-export const useAppStore = defineStore("app", () => {
-  const isLoading = ref(false);
-
-  return { isLoading };
-});
